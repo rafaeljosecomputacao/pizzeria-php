deb542b884573870daf9f0dc3dfe7d9bdb755a37
rafaeljosecomputacao <132665090+rafaeljosecomputacao@users.noreply.github.com>
2024-01-02 21:12:27 -0300

    get-data-for-select

--- index.php
+++ index.php
@@ -5,22 +5,32 @@
     <!-- Main -->
     <main class="main">
         <h2 class="text-center mb-3">Choose your pizza</h2>
-        <form action="" method="">
+        <form action="process/new-order.php" method="POST">
             <div class="form-group">
                 <label for="edge">Edge</label>
                 <select name="edge" id="edge" class="form-control mb-3">
                     <option value="">Select the edge</option>
+                    <?php foreach($edges as $e): ?>
+                        <option value="<?= $e["id"] ?>"><?= $e["edge"] ?></option>
+                    <?php endforeach; ?>
                 </select>
             </div>
             <div class="form-group">
                 <label for="dough">Dough</label>
                 <select name="dough" id="dough" class="form-control mb-3">
                     <option value="">Select the dough</option>
+                    <?php foreach($doughs as $d): ?>
+                        <option value="<?= $d["id"] ?>"><?= $d["dough"] ?></option>
+                    <?php endforeach; ?>
                 </select>
             </div>
             <div class="form-group">
                 <label for="flavors">Select one or two flavors</label>
-                <select multiple name="flavors[]" id="flavors" class="form-control"></select>
+                <select multiple name="flavors[]" id="flavors" class="form-control">
+                    <?php foreach($flavors as $f): ?>
+                        <option value="<?= $f["id"] ?>"><?= $f["flavor"] ?></option>
+                    <?php endforeach; ?>
+                </select>
             </div>
             <div class="form-group">
                 <button type="submit" class="btn btn-success mt-3 w-100"><i class="bi bi-plus"></i>New Order</button>
--- process/new-order.php
+++ process/new-order.php
@@ -1,3 +1,21 @@
 <?php
     include_once("connection-database.php");
+
+    $method = $_SERVER["REQUEST_METHOD"];
+
+    // Get data for select
+    if($method === "GET") {
+        $edgesQuery = $connection->query("SELECT * FROM edges;");
+        $edges = $edgesQuery->fetchAll();
+
+        $doughsQuery = $connection->query("SELECT * FROM doughs;");
+        $doughs = $doughsQuery->fetchAll();
+
+        $flavorsQuery = $connection->query("SELECT * FROM flavors;");
+        $flavors = $flavorsQuery->fetchAll();
+
+    // Create order
+    } else if($method === "POST") {
+
+    }
 ?>
